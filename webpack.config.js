bc79094f82cad72583dfbe93cf20562631cd716d
Matthew Shaile <matt@epicindustries.co.uk>
2024-03-22 16:36:26 +0000

    Boilerplate setup

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'static'),
+        },
+        compress: true,
+        port: 9000,
+    },
+    output: {
+        filename: 'bin.js',
+        path: path.resolve(__dirname, 'out'),
+    },
+    plugins: [new HtmlWebpackPlugin({inject: "body"})],
+};
